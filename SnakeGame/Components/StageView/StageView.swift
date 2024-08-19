2d693513b2b4c324942b3c7f57c17005c7827bbc
Ramon Jr Bahio <ramonbah.kor@gmail.com>
2024-08-19 22:35:34 +0800

    update license

--- SnakeGame/Components/StageView/StageView.swift
+++ SnakeGame/Components/StageView/StageView.swift
@@ -3,7 +3,12 @@
 //  SnakeGame
 //
 //  Created by Ramon Jr Bahio on 7/22/24.
+//  Copyright (c) 2024 Ramon Jr Bahio
 //
+//  This file is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License (CC BY-NC 4.0).
+//  You may not use this file for commercial purposes. For more details, see the LICENSE file in the repository.
+//
+//  See the full license at https://creativecommons.org/licenses/by-nc/4.0/
 
 import UIKit
 import Combine
